5f7db6ac99e878626aeb515955c8e67014269f0c
XYZ <248394081+comdigis-xyz@users.noreply.github.com>
2026-02-17 02:58:46 -0300

    General improvements and xcode project reorganization

--- ios/SampleApp/App.swift
+++ ios/SampleApp/App.swift
@@ -36,14 +36,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         configurationForConnecting connectingSceneSession: UISceneSession,
         options: UIScene.ConnectionOptions) -> UISceneConfiguration {
             // Called when a new scene session is being created.
-            // Use this method to select a configuration to create the new scene with.
             return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
 
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
         // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
 
 }
@@ -53,9 +50,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
+        // Use this method to optionally configure and attach the UIWindow to the provided UIWindowScene.
         guard let _ = (scene as? UIWindowScene) else { return }
     }
 
